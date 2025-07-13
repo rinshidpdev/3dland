10608cc153eade054d5d5a1cf5b0e00c15eb13c9
rinu1326 <nifu4040@gmail.com>
2025-07-13 15:36:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Scroll from './components/Scroll'
+import Parallax from './components/Parallax'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    
+      <>
+ <Home/>
+      </>
+    
+  )
+}
+
+export default App
